e0ff7a943da082b728c88e8390b9e537d0570ce4
Osim A <g-unit_osim@mail.ru>
2025-06-22 16:15:42 +0500

    Task54_55 implemented

---
+++ Task54_55/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = @"..\..\numbers.txt";
+
+        // Создание файла и запись чисел
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            for (int i = 1; i <= 500; i++)
+            {
+                writer.Write(i);
+                if (i < 500)
+                {
+                    writer.Write(","); // добавляем запятую между числами
+                }
+            }
+        }
+
+        Console.WriteLine("Файл numbers.txt успешно создан и заполнен числами от 1 до 500.");
+
+        // Чтение из файла
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            string content = reader.ReadToEnd();
+            Console.WriteLine("Содержимое файла numbers.txt:");
+            Console.WriteLine(content);
+        }
+    }
+}
